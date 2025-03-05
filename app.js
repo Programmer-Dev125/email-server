32f6713a3b528538068cc4a2a554481cf77a5fd1
Programmer-Dev125 <programmerdev125@gmail.com>
2025-03-05 16:27:53 -0500

    Add Server

---
+++ app.js
@@ -0,0 +1,29 @@
+import { createServer } from "node:http";
+
+const data = [
+  { id: 1, name: "John Doe" },
+  { id: 2, name: "Jane Doe" },
+  { id: 3, name: "Josh Doe" },
+  { id: 4, name: "Joe Doe" },
+];
+
+const server = createServer((req, res) => {
+  res.setHeader("access-control-allow-origin", "*");
+  res.setHeader("access-control-allow-methods", "GET, POST");
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(200);
+    res.end();
+    return;
+  }
+
+  if (req.method === "GET") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(JSON.stringify(data));
+  } else {
+    res.writeHead(405, { "content-type": "application/json" });
+    res.end(JSON.stringify({ error: "Incorrect Request Method" }));
+  }
+});
+
+export default server;
