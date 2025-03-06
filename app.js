d67e09f8917facf9ec5c2b35d2d808f002326e6d
Programmer-Dev125 <programmerdev125@gmail.com>
2025-03-05 21:16:08 -0500

    toTest

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const server = createServer((req, res) => {
-  res.setHeader("access-control-allow-origin", "http://localhost:5173/contact");
+  res.setHeader("access-control-allow-origin", "http://localhost:5173");
   res.setHeader("access-control-allow-methods", "GET, POST, OPTIONS");
   res.setHeader("access-control-allow-headers", "content-type");
 
@@ -34,7 +34,6 @@ const server = createServer((req, res) => {
         res.writeHead(400, { "content-type": "application/json" });
         res.end(JSON.stringify({ error: "Error Sending Email" }));
       }
-      server.close();
     });
   } else {
     res.writeHead(405, { "content-type": "application/json" });
