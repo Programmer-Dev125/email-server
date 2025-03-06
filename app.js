7aebc78081a4d01c09ccb739b9b406edaf29e4f9
Programmer-Dev125 <programmerdev125@gmail.com>
2025-03-06 14:02:03 -0500

    Full Server

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 import { createServer } from "node:http";
-import sgMail from "@sendgrid/mail";
+import mongoose, { Schema } from "mongoose";
+import mailer from "nodemailer";
 
 const PORT = process.env.PORT || 3000;
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const mongoUrl = process.env.MONGO_PUBLIC_URL;
+
+const conn = mongoose.createConnection(mongoUrl);
+const isCollection = conn.model(
+  "ACustomModel",
+  new Schema({
+    id: { type: Number, unique: true, required: true },
+    name: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    message: { type: String, required: true },
+  }),
+  "emails"
+);
+
+const transport = mailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: "programmerdev125@gmail.com",
+    pass: "ytez etpu tnvh ogcu",
+  },
+});
 
 const server = createServer((req, res) => {
   res.setHeader("access-control-allow-origin", "http://localhost:5173");
@@ -15,24 +36,48 @@ const server = createServer((req, res) => {
     return;
   }
 
-  if (req.method === "POST") {
+  if (req.method === "POST" && req.url === "/contact") {
     let isBody;
     req.on("data", (data) => {
       isBody = JSON.parse(data.toString());
     });
     req.on("end", async () => {
-      const isSend = await sgMail.send({
-        to: "programmerdev125@gmail.com",
-        from: isBody.email,
-        subject: isBody.subject,
-        text: isBody.name + `\n` + isBody.message,
+      const hasEmail = await isCollection.exists({
+        email: { $regex: new RegExp(`^${isBody.email}$`, "i") },
       });
-      if (isSend) {
-        res.writeHead(200, { "content-type": "application/json" });
-        res.end(JSON.stringify({ success: "Email Sent Successfully" }));
+      if (hasEmail === null) {
+        const isId = await isCollection.estimatedDocumentCount();
+        const hasInserted = await isCollection.create({
+          id: isId + 1,
+          name: isBody.name,
+          email: isBody.email,
+          message: isBody.message,
+        });
+
+        if (hasInserted) {
+          const hasMailSent = await transport.sendMail({
+            to: isBody.email,
+            from: process.env.EMAIL_USER,
+            subject: "From. Abdul Ahad",
+            text: "Hello! Your message have been received will contact you shortly after. From Abdul Ahad",
+          });
+          if (hasMailSent) {
+            res.writeHead(201, {
+              "content-type": "application/json",
+            });
+            res.end(
+              JSON.stringify({ success: "Your mail has already been sent" })
+            );
+          }
+        } else {
+          res.writeHead(400, { "content-type": "application/json" });
+          res.end(JSON.stringify({ error: "Failed to send message" }));
+        }
       } else {
-        res.writeHead(400, { "content-type": "application/json" });
-        res.end(JSON.stringify({ error: "Error Sending Email" }));
+        res.writeHead(409, { "content-type": "application/json" });
+        res.end(
+          JSON.stringify({ error: "You've previously submitted an email" })
+        );
       }
     });
   } else {
